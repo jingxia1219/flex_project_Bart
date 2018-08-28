c7bf4008a82e8aaa7d84736b9d7e6d11dedf66bb
Jing <jingxia1219@gmail.com>
2018-08-28 10:38:10 -0700

    showing sign up form on the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+    <h2>Welcome to BartVenture</h2>
+
+    <form action="/register" method="post" >
+    <label>Enter Name:
+        <input type="text" id="register-name" />
+    </label>
+    <label>
+        Enter Email:
+        <input type="text" id="register-email" />
+    </label>
+    <label>
+        Enter Password:
+        <input type="text" id="register-password" />
+    </label>
+    <label>
+        Repeat Password:
+        <input type="text" id="register-password2" />
+    </label>
+    <input type="submit" value="Create User" />
+    <div>test-urosh</div>
+    </form>
+      </div>
+    );
+  }
+}
+
+export default App;
